babe550d9b1d6663e9442ac4a56e8f9d7b668bad
fvrvz <codefaraaz@gmail.com>
2022-08-08 11:57:59 +0530

    small screen warning added

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
-const player = document.getElementById('player')
+const player = document.getElementById("player");
 
 player.onclick = () => {
-    return player.play()
-}
+  return player.play();
+};
 
 player.onmouseleave = () => {
-    return player.stop()
-}
+  return player.stop();
+};
 
 // TIMER
 // Set the date we're counting down to
 var countDownDate = new Date("Aug 20, 2022 00:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
-
+var x = setInterval(function () {
   // Get today's date and time
   var now = new Date().getTime();
-    
+
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-    
+
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
   // Output the result in an element with id="demo"
-  document.getElementById("days").innerHTML = days + (days>1 ? " days" : " day");
-  document.getElementById("time").innerHTML = + hours + (hours>1 ? " hours : " : " hour : ")
-  + minutes + (minutes>1 ? " minutes : " : " minute : ") + seconds + (seconds>1 ? " seconds" : " second");
-    
-  // If the count down is over, write some text 
+  document.getElementById("days").innerHTML =
+    days + (days > 1 ? " days" : " day");
+  document.getElementById("time").innerHTML =
+    +hours +
+    (hours > 1 ? " hours : " : " hour : ") +
+    minutes +
+    (minutes > 1 ? " minutes : " : " minute : ") +
+    seconds +
+    (seconds > 1 ? " seconds" : " second");
+
+  // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("modal").style.display = "none";
   }
 }, 1000);
-
